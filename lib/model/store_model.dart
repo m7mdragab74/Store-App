beb185cb971363ad6a1aae9021e1af5567be6d57
m7mdragab <mr341349@gmail.com>
2024-09-19 15:59:44 +0300

    done create product services and model

--- lib/model/store_model.dart
+++ lib/model/store_model.dart
@@ -1,1 +1,34 @@
-class Provider {}
+class ProductModel {
+  int id;
+  String title;
+  double price;
+  String description;
+  String category;
+  String image;
+  double rate;
+  int count;
+
+  ProductModel({
+    required this.id,
+    required this.category,
+    required this.count,
+    required this.description,
+    required this.image,
+    required this.price,
+    required this.rate,
+    required this.title,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      category: json['category'],
+      count: json['rating']['count'],
+      description: json['description'],
+      image: json['image'],
+      price: json['price'],
+      rate: json['rating']['rate'],
+      title: json['title'],
+    );
+  }
+}
--- lib/services/store_service.dart
+++ lib/services/store_service.dart
@@ -1,1 +1,23 @@
-class Provider {}
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:store_app/model/store_model.dart';
+
+class ProductServices {
+  Dio dio;
+  ProductServices(this.dio);
+  Future<ProductModel> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio.get('https://fakestoreapi.com/products');
+      ProductModel productModel = ProductModel.fromJson(response.data);
+      return productModel;
+    } on DioException catch (e) {
+      final String errorMessage = e.response?.data['error']['message'] ??
+          'oops there was an error, try later';
+      throw Exception(errorMessage);
+    } catch (e) {
+      log(e.toString());
+      throw Exception('oops there was an error, try later');
+    }
+  }
+}
