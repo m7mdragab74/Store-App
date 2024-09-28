39ffdaf6e9d3e59731dc1af7c5aa510394619445
farahosama <159581667+FarahOsama511@users.noreply.github.com>
2024-09-28 15:59:45 +0300

    change the parentwedgit in login and sign

--- lib/views/Login_Page.dart
+++ lib/views/Login_Page.dart
@@ -69,103 +69,113 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF1a2531),
-        appBar: AppBar(
-          backgroundColor: Color(0xFF1a2531),
-          title: const Text(
-            "Login page",
-            style: TextStyle(color: Colors.grey),
+      backgroundColor: Color(0xFF1a2531),
+      body: Expanded(
+        child: ListView(children: [
+          const SizedBox(
+            height: 180,
           ),
-        ),
-        body: Center(
-          child: Column(children: [
-            const Spacer(
-              flex: 3,
+          const Text(
+            textAlign: TextAlign.center,
+            "Login",
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 35,
             ),
-            Textfield(
-              onEdit: login,
-              controller: usernamecontroller,
-              title: "username",
-              obscureText: false,
-            ),
-            Textfield(
-              onEdit: login,
-              controller: passwordcontroller,
-              title: "passowrd",
-              obscureText: obscuretext,
-              obscuringchar: "*",
-              suffix: IconButton(
-                  onPressed: () {
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          Textfield(
+            onEdit: login,
+            controller: usernamecontroller,
+            title: "username",
+            obscureText: false,
+          ),
+          Textfield(
+            onEdit: login,
+            controller: passwordcontroller,
+            title: "passowrd",
+            obscureText: obscuretext,
+            obscuringchar: "*",
+            suffix: IconButton(
+                onPressed: () {
+                  setState(() {
+                    obscuretext = !obscuretext;
+                  });
+                },
+                icon: obscuretext
+                    ? const Icon(
+                        Icons.visibility_off,
+                      )
+                    : const Icon(
+                        Icons.visibility,
+                      )),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Checkbox(
+                  checkColor: Colors.white,
+                  activeColor: Colors.grey,
+                  value: remeberme,
+                  onChanged: (value) {
                     setState(() {
-                      obscuretext = !obscuretext;
+                      remeberme = value!;
                     });
-                  },
-                  icon: obscuretext
-                      ? const Icon(
-                          Icons.visibility_off,
-                        )
-                      : const Icon(
-                          Icons.visibility,
-                        )),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Row(
-              children: [
-                Checkbox(
-                    checkColor: Colors.white,
-                    activeColor: Colors.grey,
-                    value: remeberme,
-                    onChanged: (value) {
-                      setState(() {
-                        remeberme = value!;
-                      });
-                    }),
-                const Text(
-                  "Remeber me",
-                  style: TextStyle(color: Colors.grey),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            MaterialButton(
+                  }),
+              const Text(
+                "Remeber me",
+                style: TextStyle(color: Colors.grey),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 90, vertical: 50),
+            child: MaterialButton(
               padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
               color: const Color(0xFF2f3d4e).withOpacity(.5),
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
+                  borderRadius: BorderRadius.circular(50)),
               onPressed: login,
               child: const Text(
                 "Login",
                 style: TextStyle(color: Colors.grey),
               ),
             ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(80),
-              child: Row(
-                children: [
-                  const Text(
-                    "Don't have an account yet?",
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return SignupPage();
-                        }));
-                      },
-                      child: const Text(
-                        "Sign Up",
-                        style: TextStyle(color: Colors.white),
-                      ))
-                ],
-              ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(80),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  "Don't have an account yet?",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return SignupPage();
+                      }));
+                    },
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.white),
+                    ))
+              ],
             ),
-          ]),
-        ));
+          ),
+        ]),
+      ),
+    );
   }
 }
--- lib/views/signup_page.dart
+++ lib/views/signup_page.dart
@@ -18,8 +18,12 @@ class _SignupPageState extends State<SignupPage> {
   TextEditingController passwordcontroller = TextEditingController();
   TextEditingController usernamecontroller = TextEditingController();
   final formKey = GlobalKey<FormState>();
+  bool isButtonPressed = false;
 
   Future<void> signup() async {
+    setState(() {
+      isButtonPressed = true;
+    });
     if (formKey.currentState!.validate()) {
       final user = {
         "address": {
@@ -30,12 +34,12 @@ class _SignupPageState extends State<SignupPage> {
           "zipcode": "12926-3874"
         },
         "id": 1,
-        "email": emailcontroller,
-        "username": usernamecontroller,
-        "password": passwordcontroller,
+        "email": emailcontroller.text,
+        "username": usernamecontroller.text,
+        "password": passwordcontroller.text,
         "name": {
-          "firstname": firstnamecontroller,
-          "lastname": lastnamecontroller
+          "firstname": firstnamecontroller.text,
+          "lastname": lastnamecontroller.text
         },
         "phone": "1-570-236-7033",
         "__v": 0
@@ -55,117 +59,117 @@ class _SignupPageState extends State<SignupPage> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    signup();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xFF1a2531),
-        ),
-        backgroundColor: Color(0xFF1a2531),
-        body: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 100,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1a2531),
+      ),
+      backgroundColor: const Color(0xFF1a2531),
+      body: Form(
+        key: formKey,
+        child: ListView(
+          children: [
+            const SizedBox(height: 100),
+            const Text(
+              textAlign: TextAlign.center,
+              "Sign Up",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 35,
               ),
-              const Text(
-                "Sign Up",
-                style: TextStyle(color: Colors.grey, fontSize: 35),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Textfield(
+            ),
+            const SizedBox(height: 40),
+            Textfield(
+              validator: (value) {
+                if (isButtonPressed && (value == null || value.isEmpty)) {
+                  return 'Please enter a firstname';
+                }
+                return null;
+              },
+              title: "firstname",
+              obscureText: false,
+              controller: firstnamecontroller,
+            ),
+            Textfield(
+              validator: (value) {
+                if (isButtonPressed && (value == null || value.isEmpty)) {
+                  return 'Please enter a lastname';
+                }
+                return null;
+              },
+              title: "Lastname",
+              obscureText: false,
+              controller: lastnamecontroller,
+            ),
+            Textfield(
+              validator: (value) {
+                if (isButtonPressed && (value == null || value.isEmpty)) {
+                  return 'Please enter a email';
+                }
+                return null;
+              },
+              title: "Email",
+              obscureText: false,
+              controller: emailcontroller,
+            ),
+            Textfield(
+              validator: (value) {
+                if (isButtonPressed && (value == null || value.isEmpty)) {
+                  return 'Please enter a username';
+                }
+                return null;
+              },
+              title: "username",
+              obscureText: false,
+              controller: usernamecontroller,
+            ),
+            Textfield(
                 validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a firstname';
+                  if (isButtonPressed && (value == null || value.isEmpty)) {
+                    return 'Please enter a password';
                   }
                   return null;
                 },
-                title: "firstname",
-                obscureText: false,
-                controller: firstnamecontroller,
-              ),
-              Textfield(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a lastname';
-                  }
-                  return null;
-                },
-                title: "Lastname",
-                obscureText: false,
-                controller: lastnamecontroller,
-              ),
-              Textfield(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a email';
-                  }
-                  return null;
-                },
-                title: "Email",
-                obscureText: false,
-                controller: emailcontroller,
-              ),
-              Textfield(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a username';
-                  }
-                  return null;
-                },
-                title: "username",
-                obscureText: false,
-                controller: usernamecontroller,
-              ),
-              Textfield(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a password';
-                    }
-                    return null;
-                  },
-                  controller: passwordcontroller,
-                  title: "passoword",
-                  obscureText: obscurepass,
-                  obscuringchar: "*",
-                  suffix: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          obscurepass = !obscurepass;
-                        });
-                      },
-                      icon: obscurepass
-                          ? const Icon(
-                              Icons.visibility_off,
-                            )
-                          : const Icon(
-                              Icons.visibility,
-                            ))),
-              const SizedBox(
-                height: 50,
-              ),
-              MaterialButton(
+                controller: passwordcontroller,
+                title: "passoword",
+                obscureText: obscurepass,
+                obscuringchar: "*",
+                suffix: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        obscurepass = !obscurepass;
+                      });
+                    },
+                    icon: obscurepass
+                        ? const Icon(
+                            Icons.visibility_off,
+                          )
+                        : const Icon(
+                            Icons.visibility,
+                          ))),
+            const SizedBox(
+              height: 50,
+            ),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 90),
+              child: MaterialButton(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
                 color: Color(0xFF2f3d4e).withOpacity(.5),
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
+                  borderRadius: BorderRadius.circular(20),
+                ),
                 onPressed: signup,
                 child: const Text(
                   "Sign Up",
                   style: TextStyle(color: Colors.grey),
                 ),
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
